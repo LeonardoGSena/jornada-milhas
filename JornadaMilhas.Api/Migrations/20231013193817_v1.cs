ed6a8593cf2098173d02379de495a45b8a35795e
LeonardoGSena <lgomesgabriel@hotmail.com>
2023-10-13 16:42:12 -0300

    Database connection created

---
+++ JornadaMilhas.Api/Migrations/20231013193817_v1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JornadaMilhas.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class v1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Testimonial",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "NVARCHAR(120)", maxLength: 120, nullable: false),
+                    Testimony = table.Column<string>(type: "NVARCHAR(255)", maxLength: 255, nullable: false),
+                    Image = table.Column<string>(type: "VARCHAR(255)", maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Testimonial", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Testimonial");
+        }
+    }
+}
--- JornadaMilhas.Api/Program.cs
+++ JornadaMilhas.Api/Program.cs
@@ -2,8 +2,12 @@ using JornadaMilhas.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddConfiguration();
+builder.AddDatabase();
 
 var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
 
 app.MapGet("/", () => "Hello World!");
 
