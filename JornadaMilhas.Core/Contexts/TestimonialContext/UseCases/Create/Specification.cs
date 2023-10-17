9d3b12fdd9ee462ebc6f5550e8f7227b4c2eaaad
LeonardoGSena <lgomesgabriel@hotmail.com>
2023-10-17 13:59:47 -0300

    Create Testimony validations finished

--- JornadaMilhas.Core/Contexts/TestimonialContext/UseCases/Create/Specification.cs
+++ JornadaMilhas.Core/Contexts/TestimonialContext/UseCases/Create/Specification.cs
@@ -1,6 +1,17 @@
-﻿namespace JornadaMilhas.Core.Contexts.TestimonialContext.UseCases.Create;
+﻿
+using Flunt.Notifications;
+using Flunt.Validations;
 
-public class Specification
+namespace JornadaMilhas.Core.Contexts.TestimonialContext.UseCases.Create;
+
+public static class Specification
 {
-
+    public static Contract<Notification> Ensure(Request request)
+    => new Contract<Notification>()
+        .Requires()
+        .IsLowerThan(request.Name.Length, 101, "Name", "O nome deve ter no máximo 100 caracteres")
+        .IsGreaterThan(request.Name.Length, 2, "Name", "O nome deve ter no mínimo 3 caracteres")
+        .IsLowerThan(request.Testimony.Length, 501, "Testimony", "O depoimento deve ter no máximo 500 caracteres")
+        .IsGreaterThan(request.Testimony.Length, 2, "Testimony", "O depoimento deve ter no mínimo 3 caracteres")
+        .IsNotEmpty(request.Image, "Image");
 }
