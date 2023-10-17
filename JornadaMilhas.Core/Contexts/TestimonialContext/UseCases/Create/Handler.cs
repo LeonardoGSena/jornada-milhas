83a1d52cb0be1a7f21b1a0967a582e3383fb85b0
LeonardoGSena <lgomesgabriel@hotmail.com>
2023-10-17 14:11:03 -0300

    Create Testimony Handle started

--- JornadaMilhas.Core/Contexts/TestimonialContext/UseCases/Create/Handler.cs
+++ JornadaMilhas.Core/Contexts/TestimonialContext/UseCases/Create/Handler.cs
@@ -1,6 +1,30 @@
-﻿namespace JornadaMilhas.Core.Contexts.TestimonialContext.UseCases.Create;
+﻿using JornadaMilhas.Core.Contexts.TestimonialContext.UseCases.Create.Contracts;
+
+namespace JornadaMilhas.Core.Contexts.TestimonialContext.UseCases.Create;
 
 public class Handler
 {
+    private readonly IRepository _repository;
 
+    public Handler(IRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Response> Handle(Request request)
+    {
+        #region 01. Validar requisicao
+
+        #endregion
+
+        #region 02. Gerar Objetos
+
+        #endregion
+
+        #region 03. Persistir dados
+
+        #endregion
+
+        return null;
+    }
 }
